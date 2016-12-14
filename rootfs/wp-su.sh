3e147cdc324a5ffbab098b32c1bc829eeddf3183
Carlos Castellanos Vera <ccverak@gmail.com>
2016-12-14 17:02:48 +0100

    WP bitnami

---
+++ rootfs/wp-su.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This is a wrapper so that wp-cli can run as the www-data user so that permissions
+# remain correct
+# sudo -u www-data /usr/local/bin/wp-cli.phar $*
+/usr/local/bin/wp-cli.phar $*
